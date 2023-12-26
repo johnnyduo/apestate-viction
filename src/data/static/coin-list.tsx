e93b0477c10eab58dce55e3a52485bdad6d6062e
Chomtana <Chomtana001@gmail.com>
2023-12-26 23:06:11 +0700

    Finish ApeState frontend

---
+++ src/data/static/coin-list.tsx
@@ -0,0 +1,164 @@
+import { Bitcoin } from '@/components/icons/bitcoin';
+import { Ethereum } from '@/components/icons/ethereum';
+import { Tether } from '@/components/icons/tether';
+import { Bnb } from '@/components/icons/bnb';
+import { Usdc } from '@/components/icons/usdc';
+import { Cardano } from '@/components/icons/cardano';
+import { Doge } from '@/components/icons/doge';
+
+export const usdCoinList = [
+  {
+    icon: <Usdc />,
+    code: 'USDC',
+    name: 'USD Coin',
+    price: 1.001,
+  },
+];
+
+export const coinList = [
+  {
+    icon: <Bitcoin />,
+    code: 'D1',
+    name: 'DISTRICT 1',
+    price: 19076.29,
+  },
+  {
+    icon: <Tether />,
+    code: 'D2',
+    name: 'DISTRICT 2',
+    price: 0.999,
+  },
+  {
+    icon: <Cardano />,
+    code: 'D3',
+    name: 'DISTRICT 3',
+    price: 0.448,
+  },
+  {
+    icon: <Bnb />,
+    code: 'D4',
+    name: 'DISTRICT 4',
+    price: 214.96,
+  },
+  {
+    icon: <Bitcoin />,
+    code: 'D5',
+    name: 'DISTRICT 5',
+    price: 19076.29,
+  },
+  {
+    icon: <Tether />,
+    code: 'D6',
+    name: 'DISTRICT 6',
+    price: 0.999,
+  },
+  {
+    icon: <Cardano />,
+    code: 'D7',
+    name: 'DISTRICT 7',
+    price: 0.448,
+  },
+  {
+    icon: <Bnb />,
+    code: 'D8',
+    name: 'DISTRICT 8',
+    price: 214.96,
+  },
+  {
+    icon: <Bitcoin />,
+    code: 'D9',
+    name: 'DISTRICT 9',
+    price: 19076.29,
+  },
+  {
+    icon: <Tether />,
+    code: 'D10',
+    name: 'DISTRICT 10',
+    price: 0.999,
+  },
+  {
+    icon: <Cardano />,
+    code: 'D11',
+    name: 'DISTRICT 11',
+    price: 0.448,
+  },
+  {
+    icon: <Bnb />,
+    code: 'D12',
+    name: 'DISTRICT 12',
+    price: 214.96,
+  },
+  {
+    icon: <Bitcoin />,
+    code: 'BINHC',
+    name: 'Bình Chánh',
+    price: 214.96,
+  },
+  {
+    icon: <Tether />,
+    code: 'CAN',
+    name: 'Cần Giờ',
+    price: 214.96,
+  },
+  {
+    icon: <Cardano />,
+    code: 'GO',
+    name: 'Gò Vấp',
+    price: 214.96,
+  },
+  {
+    icon: <Bnb />,
+    code: 'HOC',
+    name: 'Hóc Môn',
+    price: 214.96,
+  },
+  {
+    icon: <Bitcoin />,
+    code: 'NHA',
+    name: 'Nhà Bè',
+    price: 214.96,
+  },
+  {
+    icon: <Tether />,
+    code: 'THU',
+    name: 'Thủ Đức',
+    price: 214.96,
+  },
+  {
+    icon: <Cardano />,
+    code: 'BINH',
+    name: 'Bình Thạnh',
+    price: 214.96,
+  },
+  {
+    icon: <Bnb />,
+    code: 'TANP',
+    name: 'Tân Phú',
+    price: 214.96,
+  },
+  {
+    icon: <Bitcoin />,
+    code: 'TANB',
+    name: 'Tân Bình',
+    price: 214.96,
+  },
+  {
+    icon: <Tether />,
+    code: 'CU',
+    name: 'Củ Chi',
+    price: 214.96,
+  },
+  // {
+  //   icon: <Usdc />,
+  //   code: 'USDC',
+  //   name: 'USD Coin',
+  //   price: 1.001,
+  // },
+
+  // {
+  //   icon: <Doge />,
+  //   code: 'DOGE',
+  //   name: 'Doge Coin',
+  //   price: 0.065,
+  // },
+];
---
+++ src/data/static/token-data.tsx
@@ -0,0 +1,85 @@
+import { Bitcoin } from '@/components/icons/bitcoin';
+import { Ethereum } from '@/components/icons/ethereum';
+import { Tether } from '@/components/icons/tether';
+import { Bnb } from '@/components/icons/bnb';
+import { Usdc } from '@/components/icons/usdc';
+import { Cardano } from '@/components/icons/cardano';
+import { Doge } from '@/components/icons/doge';
+
+export const TopTokensData = [
+  {
+    icon: <Bitcoin />,
+    code: 'D1',
+    name: 'PhayaThai',
+    volume: '256.5M',
+  },
+  {
+    icon: <Ethereum />,
+    code: 'D2',
+    name: 'Bangkapi',
+    volume: '740.7M',
+  },
+  {
+    icon: <Tether />,
+    code: 'D3',
+    name: 'Thonglor',
+    volume: '566.2M',
+  },
+  {
+    icon: <Bnb />,
+    code: 'D4',
+    name: 'Sathorn',
+    volume: '396.4M',
+  },
+  {
+    icon: <Usdc />,
+    code: 'USDC',
+    name: 'USD Coin',
+    volume: '145.1M',
+  },
+  {
+    icon: <Cardano />,
+    code: 'D5',
+    name: 'LatPrao',
+    volume: '267.3M',
+  },
+  {
+    icon: <Doge />,
+    code: 'D6',
+    name: 'Thai Baht',
+    volume: '421.9M',
+  },
+];
+
+export const TopPoolsData = [
+  {
+    from: 'D1',
+    to: 'USDC',
+    volume: '434.2M',
+  },
+  {
+    from: 'D2',
+    to: 'USDC',
+    volume: '356.4M',
+  },
+  {
+    from: 'D3',
+    to: 'USDC',
+    volume: '295.6M',
+  },
+  {
+    from: 'D4',
+    to: 'USDC',
+    volume: '402.5M',
+  },
+  {
+    from: 'D5',
+    to: 'USDC',
+    volume: '590.2M',
+  },
+  {
+    from: 'D6',
+    to: 'USDC',
+    volume: '104.6M',
+  },
+];
